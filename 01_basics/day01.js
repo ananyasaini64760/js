5c59543583d021a32c43152515be1ebed2510733
Ananya Saini <ananyasaini050@gmail.com>
2025-08-07 14:13:26 +0000

    changes made

--- 01_basics/day01.js
+++ 01_basics/day01.js
@@ -1,2 +1,8 @@
 console.log("Hello World!");
 
+const accountId = 45513
+var accountEmail = "ananya@gmail.com"
+accountPassword = 121212
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
