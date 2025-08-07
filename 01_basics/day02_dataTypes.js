619fa3bee76b4dce01fe0c5f0dcd6b35c8902cb4
Ananya Saini <ananyasaini050@gmail.com>
2025-08-07 14:20:58 +0000

    changes made

---
+++ 01_basics/day02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//alert( 3 + 3 )  We are using node.js not browser
+
+console.log(3 + 3);
+console.log("Ananya");
+
+let name = "Ananya"
+let age = 21
+let isLoggedIn = false
+
+/* data types
+
+number => 2 to the power 53
+big int
+string
+boolean
+null => standalone value
+undefinded 
+symbol - find uniqueness in different functions on react.js
+object
+
+
+*/
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
